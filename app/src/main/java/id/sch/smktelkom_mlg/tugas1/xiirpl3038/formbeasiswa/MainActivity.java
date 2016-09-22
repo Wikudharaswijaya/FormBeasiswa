de7241cb2f13696362b19a08ba2afbf6614bbe8a
WikudharaSW <shofrina99@gmail.com>
2016-09-22 10:31:39 +0700

    First Commit (+) imageview (+) Edit Text (+) Radio Button (+) Radio Button Group (+) CheckBox (+) Button (+) ScrollView

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xiirpl3038/formbeasiswa/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xiirpl3038/formbeasiswa/MainActivity.java
@@ -2,12 +2,63 @@ package id.sch.smktelkom_mlg.tugas1.xiirpl3038.formbeasiswa;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup rgStatus;
+    CheckBox cbA, cbB, cbC;
+    TextView tvHasil;
+    EditText etNama;
+    Button bOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rgStatus = (RadioGroup) findViewById(R.id.RadioGroupStatus);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        cbA = (CheckBox) findViewById(R.id.checkBox);
+        cbB = (CheckBox) findViewById(R.id.checkBox2);
+        cbC = (CheckBox) findViewById(R.id.checkBox3);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                doClick();
+            }
+        });
     }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+        String hobi = "";
+        int startlen = hobi.length();
+        if (cbA.isChecked()) hobi += cbA.getText() + ",";
+        if (cbB.isChecked()) hobi += cbB.getText() + ",";
+        if (cbC.isChecked()) hobi += cbC.getText() + ",";
+        if (hobi.length() == startlen) hobi += "Kosong";
+
+        String hasil = null;
+        if (rgStatus.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgStatus.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+        }
+        if (hasil == null) {
+            tvHasil.setText("Nama Anda : " + "Anda belum memilih jenis kelamin");
+        } else {
+            tvHasil.setText("Nama Anda : " + nama + "\n Jenis Kelamin Anda : " + hasil + "\n Hobi Anda : " + hobi);
+        }
+    }
+
 }

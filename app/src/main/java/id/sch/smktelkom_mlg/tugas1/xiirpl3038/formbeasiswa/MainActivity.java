f8e70986482614d40a1f10bb335842d291f94771
WikudharaSW <shofrina99@gmail.com>
2016-09-22 11:00:03 +0700

    First Commit (+) imageview (+) Edit Text (+) Radio Button (+) Radio Button Group (+) CheckBox (+) Button (+) ScrollView

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xiirpl3038/formbeasiswa/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xiirpl3038/formbeasiswa/MainActivity.java
@@ -1,13 +1,18 @@
 package id.sch.smktelkom_mlg.tugas1.xiirpl3038.formbeasiswa;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -61,4 +66,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.spinner_toolbar, menu);
+
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        return true;
+
+    }
 }
